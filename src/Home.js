5f4c8e2ca9649120126619fcf716cf43dac0137d
AliTaha22 <ishhtorm@hotmail.com>
2025-03-11 19:49:31 +0500

    Reused Components

--- src/Home.js
+++ src/Home.js
@@ -5,13 +5,14 @@ import BlogsList from "./BlogsList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState([
-        {title: "My new Website", body: "lorem ipsum . . . .", author: "Pudge", id: 1 },
+        {title: "My new Website", body: "lorem ipsum . . . .", author: "mario", id: 1 },
         {title: "Patch 7.3d", body: "lorem ipsum . . . .", author: "Mirana", id: 2 }, 
         {title: "Web dev tips", body: "lorem ipsum . . . .", author: "mario", id: 3 },  ]);
 
     return ( 
         <div className="home">
-            <BlogsList blogs = {blogs} blogTitle = "Welcome to my Blog"/>
+            <BlogsList blogs = {blogs} blogTitle = "All Blogs!"/>
+            <BlogsList blogs = {blogs.filter((blog) => blog.author === 'mario')} blogTitle = "Mario's Blogs"/>
         </div>
      );
 }
